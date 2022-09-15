a4bdeb0828e14af8fbf115fe0f86e1ce39694502
RakshaShejwalkar <rakshashejwalkar@gmail.com>
2022-09-16 01:42:51 +0530

    review

--- UI/react-app/src/App.js
+++ UI/react-app/src/App.js
@@ -300,6 +300,7 @@ function About()
     );
 }
 
+
 // How It Works
 function How()
 {
@@ -572,6 +573,49 @@ function CProfile()
       </div>
       </div>
   );
+}
+
+{/*review and rating*/}
+function Review()
+{
+    return(
+       
+<div className="auth-wrapper">
+         <div className="auth-inner">
+         <h3>Rate food</h3>
+         <div className="mb-3">
+    <label for="order" title="order">Order-Description</label>
+    <input type="text" className="form-control" name="review" />
+    </div>
+    
+    <div className="mb-3">    
+        <label for="review" title="review">Review</label>
+        <input type="text" className="form-control" name="review" placeholder='write here' maxLength={255} />
+        </div>
+        
+    <div class="rate" >
+    <input type="radio" id="star5" name="rate" value="5" />
+    <label for="star5" title="text">5 stars</label>
+    <input type="radio" id="star4" name="rate" value="4" />
+    <label for="star4" title="text">4 stars</label>
+    <input type="radio" id="star3" name="rate" value="3" />
+    <label for="star3" title="text">3 stars</label>
+    <input type="radio" id="star2" name="rate" value="2" />
+    <label for="star2" title="text">2 stars</label>
+    <input type="radio" id="star1" name="rate" value="1" />
+    <label for="star1" title="text">1 star</label>
+  </div><br/>
+<br/>
+  <div className="d-grid">
+              <li class="nav-item">
+                <a class="main-btn btn-hover wow fadeInUp" data-wow-delay=".6s">Submit</a>
+                </li>
+              </div>
+
+    </div>
+  
+</div>
+    );
 }
 
 //Homemaker posting
