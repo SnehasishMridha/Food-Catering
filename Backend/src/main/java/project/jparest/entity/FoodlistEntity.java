0f178e1d2e06850c2bfe4d1cfd8e744876866d94
Hitesh Pande <hiteshpande1998@gmail.com>
2022-09-18 17:14:44 +0530

    food_list & order entity added

---
+++ Backend/src/main/java/project/jparest/entity/FoodlistEntity.java
@@ -0,0 +1,216 @@
+package project.jparest.entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import java.sql.Blob;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="food_list_tbl")
+public class FoodlistEntity {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="food_id",unique=true)
+	private int id;
+	
+	@Lob
+	@Column(name="food_image")
+	private Blob image ;
+	
+	@Column(name="food_name")
+	private String name ;
+	
+	@Column(name="food_description")
+	private String data ;
+	
+	@Column(name="food_price")
+	private int price ;
+
+	@Column(name="food_category")
+	private String category ;
+
+	@Column(name="qnt")
+	private int  qnt ;
+
+	@Column(name="unit")
+	private Time unit ;
+	
+	@Column(name="valid_till_date")
+	private Date date ;     
+	
+	@Column(name="valid_till_time")
+	private Time time ; 
+	
+	@ManyToMany
+	@JoinTable(name = "food_order_list",joinColumns = {@JoinColumn(name="food_id")},inverseJoinColumns = @JoinColumn(name="order_id"))
+	private List<OrderEntity> orders1;
+	
+	@ManyToOne(cascade = CascadeType.ALL)  //consumer added
+	@JoinColumn(name="consumer_id")
+	private ConsumerEntity consumer;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="homemaker_id")
+	private HomemakerEntity homemaker;
+
+	@OneToMany(mappedBy="foodList",cascade = CascadeType.ALL)
+    private List<OrderDetailsEntity> orders;
+
+//	@ManyToOne(cascade = CascadeType.ALL)
+//	@JoinColumn(name="cart_id")
+//    private CartEntity cart;
+
+	
+	
+	public FoodlistEntity() {}
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public Blob getImage() {
+		return image;
+	}
+
+
+	public void setImage(Blob image) {
+		this.image = image;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getData() {
+		return data;
+	}
+
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+	public String getCategory() {
+		return category;
+	}
+
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+
+	public int getQnt() {
+		return qnt;
+	}
+
+
+	public void setQnt(int qnt) {
+		this.qnt = qnt;
+	}
+
+
+	public Time getUnit() {
+		return unit;
+	}
+
+
+	public void setUnit(Time unit) {
+		this.unit = unit;
+	}
+
+
+	public Date getDate() {
+		return date;
+	}
+
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+
+	public Time getTime() {
+		return time;
+	}
+
+
+	public void setTime(Time time) {
+		this.time = time;
+	}
+
+
+	public ConsumerEntity getConsumer() {
+		return consumer;
+	}
+
+
+	public void setConsumer(ConsumerEntity consumer) {
+		this.consumer = consumer;
+	}
+
+
+	public HomemakerEntity getHomemaker() {
+		return homemaker;
+	}
+
+
+	public void setHomemaker(HomemakerEntity homemaker) {
+		this.homemaker = homemaker;
+	}
+
+
+	public List<OrderDetailsEntity> getOrders() {
+		return orders;
+	}
+
+
+	public void setOrders(List<OrderDetailsEntity> orders) {
+		this.orders = orders;
+	}
+
+	
+	
+	
+	
+
+}
---
+++ Backend/src/main/java/project/jparest/entity/OrderEntity.java
@@ -0,0 +1,155 @@
+package project.jparest.entity;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="order_tbl")
+public class OrderEntity {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="order_id")
+	private int order_id;
+
+	@Column(name="total_amount")
+	private int total_amount;
+
+	@Column(name="order_status")
+	private String order_status;
+
+	@Column(name="shipping_address")
+	private String  shipping_address;
+
+	@Column(name="order_date")
+	private Date order_date;
+
+	@Column(name="order_time")
+	private Time order_time;
+
+	//	@OneToOne
+	//	private CartEntity cart;
+
+	@ManyToMany(mappedBy = "orders1")
+	private List<FoodlistEntity> foodlist;
+
+	@ManyToOne
+	@JoinColumn(name="employee_id")
+	private EmployeeEntity emp;
+
+	@ManyToOne
+	@JoinColumn(name="consumer_id")
+	private ConsumerEntity consumer;
+
+	@OneToMany(mappedBy="order", cascade = CascadeType.ALL)
+	private List<OrderDetailsEntity> orders;
+
+	public OrderEntity()
+	{}
+
+	public OrderEntity(int order_id, int total_amount, String order_status, String shipping_address, Date order_date,
+			Time order_time) {
+		super();
+		this.order_id = order_id;
+		this.total_amount = total_amount;
+		this.order_status = order_status;
+		this.shipping_address = shipping_address;
+		this.order_date = order_date;
+		this.order_time = order_time;
+	}
+
+	public int getOrder_id() {
+		return order_id;
+	}
+
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+
+	public int getTotal_amount() {
+		return total_amount;
+	}
+
+	public void setTotal_amount(int total_amount) {
+		this.total_amount = total_amount;
+	}
+
+	public String getOrder_status() {
+		return order_status;
+	}
+
+	public void setOrder_status(String order_status) {
+		this.order_status = order_status;
+	}
+
+	public String getShipping_address() {
+		return shipping_address;
+	}
+
+	public void setShipping_address(String shipping_address) {
+		this.shipping_address = shipping_address;
+	}
+
+	public Date getOrder_date() {
+		return order_date;
+	}
+
+	public void setOrder_date(Date order_date) {
+		this.order_date = order_date;
+	}
+
+	public Time getOrder_time() {
+		return order_time;
+	}
+
+	public void setOrder_time(Time order_time) {
+		this.order_time = order_time;
+	}
+
+	public EmployeeEntity getEmp() {
+		return emp;
+	}
+
+	public void setEmp(EmployeeEntity emp) {
+		this.emp = emp;
+	}
+
+	public ConsumerEntity getConsumer() {
+		return consumer;
+	}
+
+	public void setConsumer(ConsumerEntity consumer) {
+		this.consumer = consumer;
+	}
+
+	public List<OrderDetailsEntity> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<OrderDetailsEntity> orders) {
+		this.orders = orders;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderEntity [order_id=" + order_id + ", total_amount=" + total_amount + ", order_status=" + order_status
+				+ ", shipping_address=" + shipping_address + ", order_date=" + order_date + ", order_time=" + order_time
+				+ ", emp=" + emp + ", consumer=" + consumer + ", orders=" + orders + "]";
+	}
+
+
+
+}	
