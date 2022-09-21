a710127b773b19c30d6997f787ce9e33cc1d3cd2
Pritam <pritamjadhav510@gmail.com>
2022-09-21 13:41:05 +0530

    React Components

--- UI/react-app/src/App.js
+++ UI/react-app/src/App.js
@@ -8,7 +8,7 @@ import CNavBar from "./components/CNavBar";
 import HMNavBar from "./components/HMNavBar";
 import Footer from "./components/Footer";
 import Scroll from "./components/Scroll";
-
+import Home from "./components/Home";
 
 import {
   BrowserRouter as Router,
@@ -79,67 +79,8 @@ const App = () => {
 
 
 
-
 // Home page
-const Home = () => {
-  return (
-    <div>
-      <section id="home" class="hero-section">
-        <div class="hero-shape">
-          <img src="assets/img/hero/hero-shape.svg" alt="" class="shape" />
-        </div>
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-lg-6">
-              <div class="hero-content">
-                <h1 class="wow fadeInUp" data-wow-delay=".2s">
-                  Delicious Home-made Food <span>At Your Doorstep</span>{" "}
-                </h1>
-                <p class="wow fadeInUp" data-wow-delay=".4s">
-                  Register with us and have the yummiest homemade food at your
-                  door now.
-                </p>
-                <div className="d-grid">
-                  <li class="nav-item">
-                    <Link
-                      className="nav-link"
-                      to={"/Posting"}
-                      class="main-btn btn-hover wow fadeInUp"
-                      data-wow-delay=".6s"
-                    >
-                      Home-Maker
-                    </Link>
-                    &nbsp;
-                    <Link
-                      className="nav-link"
-                      to={"/Foodlist"}
-                      class="main-btn btn-hover wow fadeInUp"
-                      data-wow-delay=".6s"
-                    >
-                      Consumer
-                    </Link>
-                    
-                  </li>
-                </div>
-              </div>
-            </div>
-           
-            <div class="col-lg-6">
-              <div class="hero-img wow fadeInUp" data-wow-delay=".5s">
-                <img src="assets/img/hero/hero-img.svg" alt="" />
-              </div>
-              
-            </div>
-          </div>
-        </div>
-      </section>
-      <Services />
-      <About />
-      <How />
-      <Received/>
-    </div>
-  );
-};
+
 
 // Services
 const Services = () => {
