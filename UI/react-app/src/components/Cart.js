08156e9961e54a5c78b98bf8b9384bfbf785d150
Pritam <pritamjadhav510@gmail.com>
2022-09-21 14:58:10 +0530

    React Components

---
+++ UI/react-app/src/components/Cart.js
@@ -0,0 +1,40 @@
+//import logo from "./logo.svg";
+//import "./App.css";
+//import { useEffect,useState } from "react";
+//import axios from "axios";
+import { Link, } from 'react-router-dom'
+
+//Cart Page
+const Cart = () => {
+    return(
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <h2>CART</h2>
+          <div class = "container border border-secondary">
+            <pre>
+              <h4>1. Samosa - </h4>
+               <h4>Homemaker Name</h4>
+              <h5>Quantity : 1 <br/> Price :&#x20b9;210</h5>
+               
+               <hr></hr>
+               <h4>2. Noodle - </h4>
+               <h4>Homemaker Name</h4>
+              <h5>Quantity : 1 <br/> Price : &#x20b9;290</h5>
+  
+              <hr></hr>
+              <h5>Total Price: &#x20b9;500</h5>
+  
+            </pre>
+          </div>
+          <div>
+          <button type="button" class="btn btn-success">
+                Place Order Now
+              </button>
+          </div>
+  
+        </div>
+        </div>
+    );
+  };
+
+export default Cart;
