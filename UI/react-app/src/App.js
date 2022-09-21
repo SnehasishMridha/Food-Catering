5e964f1ae0cfec6faaae547d907cc529650ebb18
Pritam <pritamjadhav510@gmail.com>
2022-09-21 14:44:00 +0530

    React Components

--- UI/react-app/src/App.js
+++ UI/react-app/src/App.js
@@ -16,6 +16,7 @@ import CProfile from "./components/CProfile";
 import SearchBox from "./components/SearchBox";
 import FoodList from "./components/FoodList";
 import OrderHistory from "./components/OrderHistory";
+import OrderSummary from "./components/OrderSummary";
 
 import {
   BrowserRouter as Router,
@@ -402,61 +403,8 @@ function PostPage() {
 
 
 
+//Consumer OrderSummary
 
-{
-  /*Consumer OrderSummary*/
-}
-const OrderSummary = () => {
-  return (
-    <div class="row-1">
-      <div className="auth-wrapper">
-        <div className="auth-inner">
-          <div class="row container align-items-left">
-            <ul class="list-group  align-self-left">
-              <li class="list-group-item d-flex justify-content-between align-items-center">
-                Samosa --- &#x20b9;210
-              </li>
-              <li class="list-group-item d-flex justify-content-between align-items-center">
-                Veg-Thali --- &#x20b9;550
-              </li>
-              <li class="list-group-item d-flex justify-content-between align-items-center">
-                Veg-Crisp-Burger --- &#x20b9;350
-                <span class="badge badge-primary badge-pill"> </span>
-              </li>
-            </ul>
-          </div>
-        
-      {/*Deliver Address Selection */}
-      <div class="container">
-        
-            <div class="row-1">
-              
-                <ul class="list-group">
-                  <li class="list-group-item  align-items-left">
-                    <li>Delivery Address</li>
-                  </li>
-                </ul>
-
-                {/*Consumer Place Order & Total*/}
-                <div >
-                      <pre><li><h5>
-                         TOTAL Price:  &#x20b9;1110
-                        <br/>Payment:  COD(Cash On Delivery)
-                      </h5>
-                      </li>
-                      </pre>
-
-                      <button type="button" class="btn btn-success">
-                        PlaceOrder
-                      </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-  );
-};
 
 //OrderPlaced
 const OrderPlaced = () => {
