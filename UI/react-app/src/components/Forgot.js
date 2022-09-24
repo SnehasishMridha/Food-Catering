d14b78ae0859fc70975834b6da2d986c87788737
Hitesh Pande <hiteshpande1998@gmail.com>
2022-09-24 22:13:02 +0530

    Forgot componenet modified

--- UI/react-app/src/components/Forgot.js
+++ UI/react-app/src/components/Forgot.js
@@ -1,39 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import '../App.css';
-
+import axios from 'axios';
+import { useNavigate } from "react-router";
 const Forgot = () => {
+
+    let [pwd,setPwd]=useState("");
+    let [email,setEmail]=useState("");
+    let navigate=useNavigate()
+    function pwdHandler()
+    {       
+ 
+  
+   axios.post('http://localhost:8080/signin/changepwd/'+email+'/'+pwd,{})
+   .then((response) =>{
+     console.log(response.data)
+    if(response.data ===null )
+    {
+     alert("failed");
+    }
+    else
+    {     
+     console.log(response.data);
+     alert("password has been changed succesfully")
+
+     navigate("/sign-in")
+    }
+   }).catch(error => {console.log(error); alert("error occured") });
+
+    }
     return(
         <div className="auth-wrapper">
         <div className="auth-inner">
         <form>
             <h3>Forgot Password</h3>
-            <h4>Forgot your password?</h4><br/>
-            <p>Change your password in three easy steps. This will help you to secure your password!</p>
-            <ol class="list-unstyled">
-                <li>&nbsp;&nbsp;<span>1. </span>&nbsp;Enter your email address below.</li>
-                <li>&nbsp;&nbsp;<span>2. </span>&nbsp;Our system will send you a temporary link</li>
-                <li>&nbsp;&nbsp;<span>3. </span>&nbsp;Use the link to reset your password</li>
-            </ol><br/>
+           
             <div className="mb-3">
                 <label>Email address</label>
                 <input
                 type="email"
                 className="form-control"
-                placeholder="Enter your email address"
+                placeholder="Enter your email address" onBlur={(e)=>{setEmail(e.target.value)}}
                 required
                 />
-                <small class="form-text text-muted">Enter the email address you used during the registration on BhookhLagi.com. Then we'll email a link to this address.</small>
-            </div>
-
-            <div>
-                <a href="https://play.google.com/store/apps" class="main-btn-2">Get New Password</a>
-                {/*<button type="submit" className="main-btn-2 "> 
-                Get New Password 
-                </button>*/}&nbsp;&nbsp;
-                <a href="/sign-in" class="main-btn-2">Back to Login</a>
-                {/*<button type="submit" className="main-btn-2 "> 
-                Back to Login 
-                </button>*/}
+                 <label>new password</label>
+                <input
+                type="password"
+                className="form-control"
+                placeholder="Enter your new password" onBlur={(e)=>{setPwd(e.target.value)}}
+                required
+                />
+                <button type="button" className="main-btn-2 " onClick={pwdHandler}> 
+               set password
+               <br/>
+                </button>            
             </div><br/>
 
         </form>
